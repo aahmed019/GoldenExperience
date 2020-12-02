910e8e482409c7a7494e52ee8248ca86c05462eb
Ravid Rahman <ravidrahman@gmail.com>
2020-12-02 04:12:41 -0500

    basic skeleton added and page mapped out

--- src/components/ComplaintPage/ComplaintPage.js
+++ src/components/ComplaintPage/ComplaintPage.js
@@ -3,47 +3,64 @@ import Footer from '../Footer/Footer.js'
 import {Link} from 'react-router-dom';
 import {Row} from "react-bootstrap";
 import './ComplaintPage.css'
+import queryString from 'query-string'
 
 class ComplaintPage extends Component{
-
     render(){
-        console.log(this.state)
-        return(
-            <div>
-            <div className ='background-boi'>
-                <div className = "container">
-                    <div className = "complaint-form">
-                        {/* <Row>
-                            <Col md = {12} lg= {12}>
-                                <div className ="login-title">Login</div><br></br>
-                            </Col>
-                            <Col md = {12} lg= {12}>
-                                <div className = "user">Username*<br/></div>
-                                <input type = "user-input" className="login-input-field" value ={this.state.value} onChange={this.handleChange} />
-                            </Col>
-                            <Col md = {12} lg= {12}>
-                                <div className = "password">Password *<br/></div>
-                                <input type = "password-input" className="login-input-field" passw ={this.state.passw} onChange={this.handleChangepassw} />
-                            </Col>
-                            <Col md = {12} lg= {12}>
-                                <button className = "sign-in" disabled={!this.validateForm()}><Link to ={{pathname: "/" }}>Sign in </Link></button>&nbsp;&nbsp;
-                                <button className ="sign-up" style={{color:"white"}}><Link to ={{pathname: "/Register" }} >Sign Up</Link><br/></button>
-                                <button className ="sign-up" onClick={() => {this.getMarker()} } style={{color:"red"}}>test</button>
-                            </Col>
-                        </Row> */}
-                        <form>
-                            <label for="title">Title</label><br></br>
-                            <input type="text" id="title" name="title"></input><br></br>
-                            <label for="complain">Complain</label><br></br>
-                            <input type="text" id="complain" name="complain"></input><br></br>
-                            <input type="submit" value="Submit"></input>
-                        </form>
+        const value = queryString.parse(this.props.location.search);
+        const usertype = value.usertype;
+        const username = value.username;
+        console.log(usertype);
+        console.log(username);
+        console.log(this.state);
+        var showComplimentForm = false;
+        var showComplaintForm = false;
+        if(usertype === "manager"){
+
+        }else{
+            return(
+                <div>
+                <div className ='background-boi'>
+                    <div className = "container">
+                        <div class="row" style = {{backgroundColor: "green"}}>
+                            <div class="column" style = {{backgroundColor: "blue"}} >
+                                <h2>Submit Compliment</h2>
+                                <form>
+                                    <h3>username:</h3>
+                                    <input type="text" compliment_user="compliment_user"/><br></br>
+                                    <input type="button" value="Submit" onClick={() => showComplimentForm = true}/>
+                                </form>
+                                <div id="compliment_form" style={{ display: showComplimentForm ? "block" : "none" }}>
+                                    <p>postulant periculis vel an, mea modus similique no.</p><br></br>
+                                    <p>Eam oporteat oportere scribentur ad, mel ea oporteat probatus.</p><br></br>
+                                    <p>At offendit suscipiantur pri, dicant corrumpit id vis.</p><br></br>
+                                    <p>Odio fierent imperdiet duo cu, ad democritum appellantur duo, ex ancillae conceptam pri.</p><br></br>
+                                </div>
+                            </div>
+                            <div class="column" style = {{backgroundColor: "purple"}}>
+                                <h2>Submit Complaint</h2>
+                                <form>
+                                    <h3>username:</h3>
+                                    <input type="text" complaint_user="complaint_user"/><br></br>
+                                    <input type="button" value="Submit" onClick={() => showComplaintForm = true}/>
+                                </form>
+                                <div id="complaint_form" style={{ display: showComplaintForm ? "block" : "none" }}>
+                                    <p>postulant periculis vel an, mea modus similique no.</p><br></br>
+                                    <p>Eam oporteat oportere scribentur ad, mel ea oporteat probatus.</p><br></br>
+                                    <p>At offendit suscipiantur pri, dicant corrumpit id vis.</p><br></br>
+                                    <p>Odio fierent imperdiet duo cu, ad democritum appellantur duo, ex ancillae conceptam pri.</p><br></br>
+                                </div>
+                            </div>
+                        </div>
+                        <div style = {{backgroundColor: "red"}}>
+                                <h1 style = {{verticalAlign: "top"}}>Display active complaint threads here</h1>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <Footer/>
-            </div>
-        )
+                <Footer/>
+                </div>
+            )
+        }
     }
 }
 export default ComplaintPage
