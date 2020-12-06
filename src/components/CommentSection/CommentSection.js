65fd19550b69eb91d94e1a6acf03905d0a1718a3
Eram M <eram.manasia@gmail.com>
2020-12-05 21:04:12 -0500

    Insert successful

--- src/components/CommentSection/CommentSection.js
+++ src/components/CommentSection/CommentSection.js
@@ -1,25 +1,102 @@
-import React, { useState } from 'react';
+import React, {useEffect,  useState } from 'react';
 import {Button, Form} from "react-bootstrap";
 import Fire from '../../firebaseConfig';
 import Comment from "../Comment/Comment"
 import './CommentSection.css'
 import Footer from "../Footer/Footer"
+import Notifications, {notify} from 'react-notify-toast';
+import { useAuth } from "../../contexts/AuthContext"
 
 export default function CommentSection(props){
     const [value, setValue] = useState(''); 
     const [comments, setComments] = useState(props.location.state.comments); 
     let database = Fire.db
+    const [username, setUsername] = useState(true);
+    const [email, setEmail] = useState(true);
+    const { currentUser, logout } = useAuth()
+    const [userAuthorize, setAuthorize] = useState(true);
+
 
     // if(props.location.state){
     //     setComments(props.location.state.comments)
     // }
+
+    const getUser = async() =>{
+        if(currentUser){
+            database.getCollection('Users').doc(currentUser.email).get().then(function(doc){
+                if(!doc.exists){
+                    setAuthorize(false);
+                }
+                else{
+                    setUsername(doc.data().username)
+                    setEmail(doc.data().email)
+                }
+            })    
+        }
+    }
+    useEffect(() =>{
+        // getPosts()
+        // getData()
+        getUser()
+    },[])
+
+    const deRegisterUser = (email) => {
+        database.getCollection('Users').doc(email).delete()
+        .then(() =>{
+            notify.show('Stop cursing! You have been deregistered!');
+            window.location.reload(false);
+        })
+        .catch(function(error) { //broke down somewhere
+            console.error("Error: ", error);
+        });
+    }
+
+    const removeVIP = (email) => {
+        database.getCollection('SignUp').doc(email).update({
+            Vip: "false",
+            warnings: 0
+          })
+        database.getCollection('Users').doc(email).update({
+            Vip: "false",
+            warnings: 0
+        }).then(() => {
+            notify.show('Stop cursing! Your VIP status has been removed!');
+        })
+    }
+
+    const addWarning = (username, email) =>{
+        database.getCollection('Users').doc(email).get().then(function(doc){
+            let new_warnings = 0;
+            let vip_status = false;
+            if(doc.exists){
+              new_warnings = doc.data().warnings + 1;
+              vip_status = doc.data().Vip;
+              console.log(vip_status)
+              database.getCollection('Users').doc(email).update({
+                warnings: new_warnings,
+              })
+              database.getCollection('SignUp').doc(email).update({
+                warnings: new_warnings,
+              })
+              if(new_warnings >= 2 && vip_status == "true"){
+                removeVIP(email);
+              }
+              else if(new_warnings >= 3 && vip_status == "false"){
+                deRegisterUser(email);
+              }else{
+                notify.show('Stop cursing! A warning has been added to your account!');
+              }
+           
+            }
+        })
+    }
 
     const handleChange = (e) =>{
         this.setState({value: e.target.value});
     }
     const handleSubmit = (e) =>{
         let tabooList = [];
-        this.db.getCollection('TabooWords').get()
+        database.getCollection('TabooWords').get()
         .then(querySnapshot => {
             querySnapshot.docs.forEach(doc => {
                 //let currentId = doc.id
@@ -27,30 +104,36 @@ export default function CommentSection(props){
                 
             });
             let violation_count = 0;
+            let new_val = value;
             for(let i = 0; i < tabooList.length; i++){
+
                 let ast_str = "";
                 for(let j = 0; j < tabooList[i].length; j++){
                     ast_str += "*";
                 }
                 // temp = tabooList[i];
-                let count = value.split(tabooList[i]).length - 1;;
+                let count = value.split(tabooList[i]).length - 1;
                 violation_count += count;
-                value = value.replaceAll(tabooList[i], ast_str);
+                new_val = new_val.replaceAll(tabooList[i], ast_str)
+                setValue(new_val)
             }
             if(value.length > 0 && violation_count <= 3){
+                if(violation_count > 0){
+                    addWarning(username, props.email);
+                }
                let prevData = props.location.state.data.posts;
                console.log(prevData);
                 for(let i = 0; i < prevData.length; i++){
-                    if(prevData[i].text === props.location.state.text && prevData[i].username === props.location.state.username){
-                        
+                    if(prevData[i].text === props.location.state.text && prevData[i].username === props.location.state.poster){
                             prevData[i].comments.push({
                                 "text": value,
-                                "username": "rudeuser",
+                                "username": username,
                                 time: database.getTime()
                             })    
                     }
                 }
-                    this.db.getCollection("Topics").doc(props.location.state.id).update({
+                console.log(prevData)
+                    database.getCollection("Topics").doc(props.location.state.id).update({
                         "posts": prevData
                     }).then(() =>{
                     console.log("New Post Added to Database");
@@ -67,6 +150,10 @@ export default function CommentSection(props){
             console.log(error)
         })
     }
+    if(userAuthorize == false){
+        return(<div>You need to be approved to view this page</div>)
+    }
+
 
         if(props.location.state){
             return(
@@ -77,7 +164,7 @@ export default function CommentSection(props){
                     </div>
                     <Form>
                         <div className="formComment">
-                            <input className="inputComment" value={value} onChange={handleChange}/>
+                            <input className="inputComment" value={value} onChange={e => setValue(e.target.value)}/>
                             <Button className="buttonComment gold-text" onClick={handleSubmit}>Post Comment</Button>
                         </div>
                     </Form>
