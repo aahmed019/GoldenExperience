9dd9f4da9c4b5c9dc5e11a4ac606e8d4fb713ce8
Eram M <eram.manasia@gmail.com>
2020-12-11 12:08:28 -0500

    Added id to chef cards

--- src/components/ChefPage/FoodItems.js
+++ src/components/ChefPage/FoodItems.js
@@ -170,6 +170,8 @@ export default function FoodItems() {
                         <Card.Title>{item.name} for ${item.price} FOR VIP ONLY</Card.Title>
                         <Card.Text>
                         {item.description}
+                        <br/>
+                        ID: {item.id}
                         </Card.Text>
                         <Button onClick={() => {deleteFoodItem(item)}}>Delete</Button>
                     </Card.Body>
@@ -183,6 +185,8 @@ export default function FoodItems() {
                         <Card.Title>{item.name} for ${item.price}</Card.Title>
                         <Card.Text>
                         {item.description}
+                        <br/>
+                        ID: {item.id}
                         </Card.Text>
                         <Button onClick={() => {deleteFoodItem(item)}}>Delete</Button>
                     </Card.Body>
