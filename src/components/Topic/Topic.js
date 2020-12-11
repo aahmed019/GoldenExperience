46cfaa438547745ca2554ec8c527195b243a66c8
Eram M <eram.manasia@gmail.com>
2020-12-10 22:09:27 -0500

    Topic is added

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -7,7 +7,7 @@ class Topic extends Component{
     render(){
         return(
         <div>
-            <p><Link className = "topic-link"to ={{pathname: "/Posts", state: {
+            <p><Link style={{fontSize: "2rem", marginLeft: "2%"}} className = "topic-link"to ={{pathname: "/Posts", state: {
     data: this.props.data,
     id : this.props.id
   } } }>{this.props.name}</Link></p>
